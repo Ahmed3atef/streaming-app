376b0383dbc60169d95f4f10c6756cd641d28e7d
Ahmed@Atef <clay674302@gmail.com>
2026-02-25 15:55:54 +0200

    feat: update settings for static files and add Whitenoise for improved static file handling

--- StreamingSite/settings.py
+++ StreamingSite/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-x$$=9as-v%o&05ts@hv*c^b2srko-^5i6*2zd_qu22)6uh4!=2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cinevault.nexus.local', '192.168.1.9', 'localhost']
+ALLOWED_HOSTS = ['nexus.local', '192.168.1.9', 'localhost']
 
 
 # Application definition
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     
     # third-party
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -139,9 +141,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Storage
 STORAGES = {
